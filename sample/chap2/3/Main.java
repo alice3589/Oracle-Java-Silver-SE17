9e69bcfc206c596af49db18314ffa403a9652c26
alice3589 <ryutoiwa2929@gmail.com>
2026-03-18 22:36:04 +0900

    Add files via upload

---
+++ sample/chap2/3/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(100);                // 10 進数
+        System.out.println(0b1100100);          // 2 進数
+        System.out.println(0144);               // 8 進数
+        System.out.println(0x64);               // 16 進数
+        System.out.println(3.1415);             // 浮動小数点
+        System.out.println(31415e-4);           // 指数表記
+        System.out.println('\u0061');           // 文字 a
+        System.out.println("\u0041\u0042\n");   // 文字列 AB と改行
+        System.out.println(true);               // 真偽地
+    }
+}
