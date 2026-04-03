d36ed44df88501d79e97cee81b0640c29138e2f7
alice3589 <ryutoiwa2929@gmail.com>
2026-04-03 22:44:13 +0900

    Add files via upload

---
+++ sample/chap2/6/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        String s = "Hello World";
+        s = "Hello";
+        s = s + " Duke";
+        System.out.println(s);
+    }
+}
